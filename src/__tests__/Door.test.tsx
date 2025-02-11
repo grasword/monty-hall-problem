ac3250c959500dd40880f58f2a594d55c42148af
Oleh Zhmaiev <grasword6@gmail.com>
2025-02-11 18:24:50 +0200

    [test] add unit tests for the `Door` component

---
+++ src/__tests__/Door.test.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { describe, expect, it, vi } from 'vitest'
+import Door from '../components/Door.jsx'
+import { testIds } from '../testIds.js'
+
+describe('Door Component', () => {
+  const handleSelect = vi.fn()
+
+  it('should render the door number', () => {
+    render(<Door doorNumber={0} isSelected={false} isWinning={false} isRevealed={false} onSelect={handleSelect} />)
+    expect(screen.getByTestId(testIds.door(0).label).textContent).toBe('1')
+  })
+
+  it('should call onSelect when clicked', () => {
+    render(<Door doorNumber={0} isSelected={false} isWinning={false} isRevealed={false} onSelect={handleSelect} />)
+    fireEvent.click(screen.getByTestId(testIds.door(0).container))
+    expect(handleSelect).toHaveBeenCalledWith(0)
+  })
+
+  it('should display a trophy if the door is winning and revealed', () => {
+    render(<Door doorNumber={0} isSelected={false} isWinning={true} isRevealed={true} onSelect={handleSelect} />)
+    expect(screen.getByTestId(testIds.door(0).label).textContent).toBe('🏆')
+  })
+
+  it('should display a goat if the door is not winning and revealed', () => {
+    render(<Door doorNumber={0} isSelected={false} isWinning={false} isRevealed={true} onSelect={handleSelect} />)
+    expect(screen.getByTestId(testIds.door(0).label).textContent).toBe('🐐')
+  })
+})
