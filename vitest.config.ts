1f6c0ec4b08e7b07335d24eba59f967b8599a7c7
Oleh Zhmaiev <grasword6@gmail.com>
2025-02-11 18:24:23 +0200

    [conf] add `vitest` configuration

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    exclude: ['build', 'node_modules'],
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    globals: true,
+    coverage: {
+      exclude: ['build', ...coverageConfigDefaults.exclude],
+      provider: 'v8'
+    },
+  },
+});
