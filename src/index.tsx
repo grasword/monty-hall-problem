8672dd4497cd020eca00f78f4157bdfa36bbfbb2
Oleh Zhmaiev <grasword6@gmail.com>
2025-02-14 17:02:57 +0200

    [misc] mount root component using astro page

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { App } from './App.jsx'
-// @ts-expect-error it's just a style
-import './styles/App.css'
-
-const root = createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
