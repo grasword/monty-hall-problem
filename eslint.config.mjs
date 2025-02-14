10581b2d8035df7570783f42685652c725b551ab
Oleh Zhmaiev <grasword6@gmail.com>
2025-02-14 15:40:27 +0200

    [conf] ignore unpublished packages that we know for sure are published

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,20 @@ export default [
   {
     rules: {
       'jsdoc/require-jsdoc': 'off',
-      'import-x/no-default-export': 'off'
+      'import-x/no-default-export': 'off',
+      'n/no-unpublished-import': [
+        'error',
+        {
+          allowModules: [
+            'vite',
+            'vitest',
+            '@vitejs/plugin-react',
+            '@wdio/globals',
+            '@testing-library/jest-dom',
+            '@testing-library/react'
+          ]
+        }
+      ]
     }
   }
 ].flat()
